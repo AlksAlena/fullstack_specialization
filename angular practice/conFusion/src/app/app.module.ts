9debad6b719b3bd791c4c61de6aeb7d6866c9681
Helen <alksalena@gmail.com>
2018-07-08 23:25:03 +0700

    added dishdetail component with card of details and block of comments

--- angular practice/conFusion/src/app/app.module.ts
+++ angular practice/conFusion/src/app/app.module.ts
@@ -9,11 +9,13 @@ import { MaterialModule } from './material.module';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
     BrowserModule,
