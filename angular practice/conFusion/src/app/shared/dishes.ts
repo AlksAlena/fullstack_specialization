1db49863ab1b5dde47430d68fcad152bfd1885da
Helen <alksalena@gmail.com>
2018-07-16 08:42:10 +0700

    fixed semicolon replaced by a comma

--- angular practice/conFusion/src/app/shared/dishes.ts
+++ angular practice/conFusion/src/app/shared/dishes.ts
@@ -8,7 +8,7 @@ export const DISHES: Dish[] = [
     category: 'mains',
     label: 'Hot',
     price: '4.99',
-    featured: true;
+    featured: true,
     // tslint:disable-next-line:max-line-length
     description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
     comments: [
@@ -51,7 +51,7 @@ export const DISHES: Dish[] = [
     category: 'appetizer',
     label: '',
     price: '1.99',
-    featured: false;
+    featured: false,
     description: 'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
     comments: [
       {
@@ -93,7 +93,7 @@ export const DISHES: Dish[] = [
     category: 'appetizer',
     label: 'New',
     price: '1.99',
-    featured: false;
+    featured: false,
     description: 'A quintessential ConFusion experience, is it a vada or is it a donut?',
     comments: [
       {
@@ -135,7 +135,7 @@ export const DISHES: Dish[] = [
     category: 'dessert',
     label: '',
     price: '2.99',
-    featured: false;
+    featured: false,
     description: 'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
     comments: [
       {
