22010310eeec3a4af86b2ca2e1a22868db1293fa
Helen <alksalena@gmail.com>
2018-07-20 21:01:21 +0700

    added RxJS, replacement Promise on the Observable

--- angular practice/conFusion/src/app/services/promotion.service.ts
+++ angular practice/conFusion/src/app/services/promotion.service.ts
@@ -3,6 +3,9 @@ import { Injectable } from '@angular/core';
 import { Promotion } from '../shared/promotion';
 import { PROMOTIONS } from '../shared/promotions';
 
+import { Observable, of } from 'rxjs';
+import { delay } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,21 +13,15 @@ export class PromotionService {
 
   constructor() { }
 
-  getPromotions(): Promise<Promotion[]> {
-    return new Promise(resolve => {
-      setTimeout(() => resolve(PROMOTIONS), 2000);
-    });
+  getPromotions(): Observable<Promotion[]> {
+    return of(PROMOTIONS).pipe(delay(2000));
   }
 
-  getPromotion(id: number): Promise<Promotion> {
-  	return new Promise(resolve => {
-      setTimeout(() => resolve(PROMOTIONS.filter((promo) => (promo.id === id))[0]), 2000);
-    });
+  getPromotion(id: number): Observable<Promotion> {
+  	return of(PROMOTIONS.filter((promo) => (promo.id === id))[0]).pipe(delay(2000));
   } 
   
-  getFeaturedPromotion(): Promise<Promotion> {
-  	return new Promise(resolve => {
-      setTimeout(() => resolve(PROMOTIONS.filter((promo) => (promo.featured))[0]), 2000);
-    });
+  getFeaturedPromotion(): Observable<Promotion> {
+  	return of(PROMOTIONS.filter((promo) => (promo.featured))[0]).pipe(delay(2000));
   } 
 }
