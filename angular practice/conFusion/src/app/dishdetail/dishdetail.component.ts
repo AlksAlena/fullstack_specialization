b2520788557b92f5da612e0732426cecd52f6767
Helen <alksalena@gmail.com>
2018-07-24 20:19:40 +0700

    added subscribe on change router.params, prev and next buttons in detail card, progress spinner

--- angular practice/conFusion/src/app/dishdetail/dishdetail.component.ts
+++ angular practice/conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -4,8 +4,9 @@ import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
 import { Dish } from '../shared/dish';
-
 import { DishService } from '../services/dish.service';
+
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dishdetail',
@@ -14,15 +15,25 @@ import { DishService } from '../services/dish.service';
 })
 export class DishdetailComponent implements OnInit {
   dish: Dish;
+  dishIds: number[];
+  prev: number;
+  next: number;
   
-  constructor(private dishservice: DishService,
+  constructor(private dishService: DishService,
     private route: ActivatedRoute,
     private location: Location) { }
 
   ngOnInit() {
-  	const id = +this.route.snapshot.params['id'];
-    this.dishservice.getDish(id)
-      .subscribe(dish => this.dish = dish);
+    this.dishService.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
+    this.route.params
+      .pipe(switchMap((params: Params) => this.dishService.getDish(+params['id'])))
+      .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
+  }
+
+  setPrevNext(dishId: number) {
+    const index = this.dishIds.indexOf(dishId);
+    this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
+    this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
   }
 
   goBack(): void {
--- angular practice/conFusion/src/app/services/dish.service.ts
+++ angular practice/conFusion/src/app/services/dish.service.ts
@@ -18,10 +18,13 @@ export class DishService {
 
   getDish(id: number): Observable<Dish> {
     return of(DISHES.filter((dish) => (dish.id === id))[0]).pipe(delay(2000));
-  } 
-  
+  }
+
   getFeaturedDish(): Observable<Dish> {
-  	return of(DISHES.filter((dish) => (dish.featured))[0]).pipe(delay(2000));
-    });
+    return of(DISHES.filter((dish) => (dish.featured))[0]).pipe(delay(2000));
   } 
+
+  getDishIds(): Observable<number[] | any> {
+    return of(DISHES.map(dish => dish.id));
+  }
 }
