12d91444950ea259a07f174c58a18937fc757eac
Helen <alksalena@gmail.com>
2018-08-03 15:01:02 +0700

    added simple module

--- nodejs practice/index.js
+++ nodejs practice/index.js
@@ -1,19 +1,16 @@
-var rect = {
-  perimeter: (x, y) => (2*(x+y)),
-  area: (x, y) => (x*y)
-};
+var rect = require('./rectangle');
 
 function solveRect(l,b) {
-    console.log("Solving for rectangle with l = " + l + " and b = " + b);
+  console.log("Solving for rectangle with l = " + l + " and b = " + b);
 
-    if (l <= 0 || b <= 0) {
-        console.log("Rectangle dimensions should be greater than zero:  l = "
-               + l + ",  and b = " + b);
-    }
-    else {
-      console.log("The area of the rectangle is " + rect.area(l,b));
-      console.log("The perimeter of the rectangle is " + rect.perimeter(l,b));
-    }
+  if (l <= 0 || b <= 0) {
+    console.log("Rectangle dimensions should be greater than zero:  l = "
+     + l + ",  and b = " + b);
+  }
+  else {
+    console.log("The area of the rectangle is " + rect.area(l,b));
+    console.log("The perimeter of the rectangle is " + rect.perimeter(l,b));
+  }
 }
 
 solveRect(2,4);
