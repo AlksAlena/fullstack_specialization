d14c74e63b4802476c0a681bea08ba1baa7aa734
Helen <alksalena@gmail.com>
2018-08-10 20:34:13 +0700

    init express-server

---
+++ nodejs practice/node-express/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const http = require('http');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log(req.headers);
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<html><body><h1>Express Server is work!</h1></body></html>');
+});
+
+const server = http.createServer(app);
+server.listen(port, hostname, () => {
+  console.log(`server running at http://${hostname}:${port}/`);
+});
