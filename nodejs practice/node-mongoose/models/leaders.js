ffebce5d3e79a574cba474c7fce01f57d33d4280
Helen <alksalena@gmail.com>
2018-08-17 13:14:55 +0700

    init node-mongoose

---
+++ nodejs practice/node-mongoose/models/leaders.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const leaderSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+},{
+    timestamps: true
+});
+
+var Leaders = mongoose.model('Leader', leaderSchema);
+
+module.exports = Leaders;
